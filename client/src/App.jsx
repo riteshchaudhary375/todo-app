e2a815323943ed14f913587da6798e5cf5071f9f
ritesh <riteshchaudhary375@gmail.com>
2024-08-05 14:46:57 +0545

    updating route of the components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AppName from "./components/AppName";
-import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import Container from "./components/Container";
 import WelcomeMessage from "./components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoNav from "./components/TodoNav";
 import "./App.css";
 
 const App = () => {
-  const [todoItems, setTodoItems] = useState([]);
-  // console.log(todoItems);
-
-  useEffect(() => {
-    fetch("/api/v1/getTodos")
-      .then((res) => res.json())
-      .then((data) => {
-        setTodoItems(data.todos);
-        // console.log(data);
-      });
-  }, [todoItems]);
-
-  /*   const handleNewItem = (itemName, itemDueDate) => {
-    const newTodoItems = [
-      ...todoItems,
-      { name: itemName, dueDate: itemDueDate },
-    ];
-    setTodoItems(newTodoItems);
-  }; */
-
-  const handleDeleteItem = (todoId) => {
-    /* const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems); */
-
-    fetch(`/api/v1/deleteTodo/${todoId}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
-    <TodoItemsContext.Provider value={todoItems}>
-      <Container>
-        <center className="todoContainer">
-          <AppName />
-          <AddTodo />
-          <WelcomeMessage />
-          <TodoItems onDeleteClick={handleDeleteItem} />
-        </center>
-      </Container>
-    </TodoItemsContext.Provider>
+    <Container>
+      <center className="todoContainer">
+        <AppName />
+        <TodoNav />
+        <TodoItems />
+        <WelcomeMessage />
+      </center>
+    </Container>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import EditTodo from "./components/EditTodo.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateTodo from "./components/CreateTodo.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TodoContextProvider from "./store/todo-items-store";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/edit-todo/:todoId" element={<EditTodo />} />
-      </Routes>
-    </BrowserRouter>
+    <TodoContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/create-todo" element={<CreateTodo />} />
+          <Route path="/edit-todo/:todoId" element={<EditTodo />} />
+        </Routes>
+      </BrowserRouter>
+    </TodoContextProvider>
   </React.StrictMode>
 );
